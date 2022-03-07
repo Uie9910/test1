ea35fab93aff0151bc749a2601ba61788eea59a4
user <user@DESKTOP-6JBEF5K>
2022-03-07 12:08:07 +0800

    102

--- test1/Program.cs
+++ test1/Program.cs
@@ -14,7 +14,9 @@ namespace test1
             int tip_precent = 15;
             int tax_precent = 8;
             var cost = meal_cost + tip_precent + (meal_cost + tip_precent) * tax_precent * 0.01;
-            Console.WriteLine("您的用餐費用是:",cost);
+            Console.WriteLine("您的用餐費用是:"+ cost);
+
+            Console.ReadLine();
         }
     }
 }
